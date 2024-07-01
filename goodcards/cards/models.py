69c0178e263472d8643673dec9f12df0d9774248
El Linzer <elinzer5@gmail.com>
2024-07-01 15:04:57 -0500

    successfully migrated

--- goodcards/cards/models.py
+++ goodcards/cards/models.py
@@ -5,10 +5,10 @@ from user.models import User
 class Card(models.Model):
     card_name = models.CharField(max_length=200)
     img_url = models.URLField()
-    mana_cost = models.CharField()
+    mana_cost = models.CharField(max_length=200)
     text = models.TextField()
-    rarity = models.CharField()
-    card_set = models.CharField()
+    rarity = models.CharField(max_length=200)
+    card_set = models.CharField(max_length=200)
 
 
 class Deck(models.Model):
