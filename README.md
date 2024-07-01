045f1d955cd74924a3da078e9e80ad654afee1ad
El Linzer <elinzer5@gmail.com>
2024-07-01 15:00:47 -0500

    made card, user, and deck models

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # goodcards2
+
+to run locally `cd` into `goodcards` and run `python manage.py runserver`
--- goodcards/cards/models.py
+++ goodcards/cards/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from user.models import User
 # Create your models here.
+
+class Card(models.Model):
+    card_name = models.CharField(max_length=200)
+    img_url = models.URLField()
+    mana_cost = models.CharField()
+    text = models.TextField()
+    rarity = models.CharField()
+    card_set = models.CharField()
+
+
+class Deck(models.Model):
+    deck_name = models.CharField(max_length=200)
+    cover_img = models.URLField()
+    cards = models.ManyToManyField(Card)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
