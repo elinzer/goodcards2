0a6291e07e26e6bd117b9fdfb8a87586725f7370
El Linzer <elinzer5@gmail.com>
2024-07-11 15:16:47 -0500

    first models made and first test written

--- goodcards/cards/models.py
+++ goodcards/cards/models.py
@@ -3,12 +3,25 @@ from user.models import User
 # Create your models here.
 
 class Card(models.Model):
+    id = models.AutoField(primary_key=True)
     card_name = models.CharField(max_length=200)
     img_url = models.URLField()
     mana_cost = models.CharField(max_length=200)
     text = models.TextField()
     rarity = models.CharField(max_length=200)
     card_set = models.CharField(max_length=200)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.card_name,
+            "img_url": self.img_url,
+            "mana": self.mana_cost,
+            "card_text": self.text,
+            "rarity": self.rarity,
+            "set": self.card_set
+        }
+
 
 
 class Deck(models.Model):
--- goodcards/cards/tests.py
+++ goodcards/cards/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from cards.models import Card
 
 # Create your tests here.
+
+class CardTestCase(TestCase):
+    def setUp(self):
+        Card.objects.create(
+            card_name="Emrakul, the World Anew",
+            img_url="https://cards.scryfall.io/large/front/b/9/b914bfd9-5be3-4459-a68a-9b3ea2747bbc.jpg?1717011239",
+            mana_cost="12",
+            text="When you cast this spell, gain control of all creatures target player controls. Flying, protection from spells and from permanents that were cast this turn. When Emrakul, the World Anew leaves the battlefield, sacrifice all creatures you control. Madness—Pay six Colorless.",
+            rarity="Mythic",
+            card_set="Modern Horizons 3"
+                            
+            )
+        
+    def test_card_to_dict(self):
+        card = Card.objects.get(card_name="Emrakul, the World Anew")
+        self.assertEqual(card.to_dict(), {
+            "id": 1,
+            "name": "Emrakul, the World Anew",
+            "img_url": "https://cards.scryfall.io/large/front/b/9/b914bfd9-5be3-4459-a68a-9b3ea2747bbc.jpg?1717011239",
+            "mana": "12",
+            "card_text": "When you cast this spell, gain control of all creatures target player controls. Flying, protection from spells and from permanents that were cast this turn. When Emrakul, the World Anew leaves the battlefield, sacrifice all creatures you control. Madness—Pay six Colorless.",
+            "rarity": "Mythic",
+            "set": "Modern Horizons 3"
+        })
+        
